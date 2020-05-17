c2e062b9806357193cd8338d5866f5d682349b53
amruthr <amruthjoshwa@gmail.com>
2020-05-17 18:28:39 +0530

    reemoved tc

--- client/travel crest/src/components/Empty.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Empty = () => <div style={{ height:'100vh', width:'100%'}}><p style={{margin:'40vh 25vw'}}>This page has not been constructed yet</p></div>;
-
-
-export default Empty;
